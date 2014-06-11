e694396d06dbf1cc63d7c26de52517ab564c5e5e
lilkraftwerk <lucasquinnmathis@gmail.com>
2014-06-11 09:48:42 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spacetweets::Application.config.secret_token = 'fca47cd0d0c3c8ea364b26e515805da413779b7fa217aeb53ead3f20258ad2327623cc90e3d483ba9ea59a797d30100b5003b81ed162f52e081eb4bd8f5c7a13'
